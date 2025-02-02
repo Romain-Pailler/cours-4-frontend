e19a3531cc70625d0e88311e4a721a30d3f8ff5b
Romain-Pailler <romain.pailler77@gmail.com>
2025-02-02 17:59:37 +0100

    modification du main et du package.json

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    define: {
+      'process.env': process.env
+    },
+
+  
   plugins: [vue()],
   server: {
     port: 3000,
